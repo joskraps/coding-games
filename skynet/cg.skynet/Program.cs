413d867373a1bcfdefac44a799d1f79fcbb9bf5c
joskraps <joskraps@users.noreply.github.com>
2018-09-01 10:41:51 -0400

    Update Program.cs
    Working

--- skynet/cg.skynet/Program.cs
+++ skynet/cg.skynet/Program.cs
@@ -1,62 +1,192 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-// ReSharper disable PossibleNullReferenceException
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
-//https://en.wikipedia.org/wiki/Breadth-first_search
-//http://www.technical-recipes.com/2015/graph-traversals-in-c-plus-plus-c-sharp/
-
-internal class Player
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        var inputs = Console.ReadLine().Split(' ');
-        var totalNodes = int.Parse(inputs[0]); // the total number of nodes in the level, including the gateways
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        var nodeCount = int.Parse(inputs[0]); // the total number of nodes in the level, including the gateways
         var linkCount = int.Parse(inputs[1]); // the number of links
-        var exitGatewayCount = int.Parse(inputs[2]); // the number of exit gateways
-        var nodes = new List<NodePoint>();
-        var entryNode = 0;
+        var exitCount = int.Parse(inputs[2]); // the number of exit gateways
+        var vertices = new int[nodeCount+1];
+        var edges = new List<Tuple<int, int>>();
+
+        for (var i = 0; i < nodeCount; i++)
+        {
+            vertices[i] = i;
+        }
 
         for (var i = 0; i < linkCount; i++)
         {
             inputs = Console.ReadLine().Split(' ');
-            var node1 = int.Parse(inputs[0]); // Point1 and Point2 defines a link between these nodes
-            var node2 = int.Parse(inputs[1]);
 
-            Console.Error.WriteLine($"Point1: {node1} Point2: {node2}");
+            var nodeLink1 = int.Parse(inputs[0]); // N1 and N2 defines a link between these nodes
+            var nodeLink2 = int.Parse(inputs[1]);
 
-            nodes.Add(new NodePoint {Point1 = node1, Point2 = node2});
+            Console.Error.WriteLine($"Node Start: {nodeLink1} Node End: {nodeLink2}");
+
+            edges.Add(new Tuple<int, int>(nodeLink1,nodeLink2));
         }
 
-        for (var i = 0; i < exitGatewayCount; i++)
+        var graph = new Graph<int>(vertices, edges);
+        var algorithms = new Algorithms();
+        var endingG = new List<int>();
+
+        for (var i = 0; i < exitCount; i++)
         {
-            var ei = int.Parse(Console.ReadLine()); // the index of a gateway node
-            Console.Error.WriteLine($"EI: {ei}");
-            entryNode = ei;
+            var EI = int.Parse(Console.ReadLine()); // the index of a gateway node
+            Console.Error.WriteLine($"Index of a gateway: {EI}");
+            endingG.Add(EI);
         }
-
-        var pointsToAdd = new List<NodePoint>();
-
+        
+        var path = new List<int>();
 
         // game loop
         while (true)
         {
-            var currentPosition = int.Parse(Console.ReadLine()); // The index of the node on which the Skynet agent is positioned this turn
-            Console.Error.WriteLine($"SI: {currentPosition}");
-
-            foreach (var result in nodes.Where(np => np.Point1 == currentPosition).ToList())
+            var SI = int.Parse(Console.ReadLine()); // The index of the node on which the Skynet agent is positioned this t
+            var maxJumps = 999;
+            var fp1 = 0;
+            var fp2 = 0;
+            
+            foreach(var ei in endingG)
             {
-                if (result.Point2 != entryNode) continue;
+                var pointsBlah = algorithms.ShortestPathFunction(graph, SI)(ei);
+                Console.Error.WriteLine($"{pointsBlah[0]} {pointsBlah[1]} {pointsBlah.Count}");
+                
+                if(pointsBlah.Count < maxJumps)
+                {
+                    fp1 = pointsBlah[0];   
+                    fp2 = pointsBlah[1];
+                    
+                    maxJumps = pointsBlah.Count;
+                }
+            }
+            
 
-                pointsToAdd.Add(result);
+            Console.Error.WriteLine($"Starting index: {SI}");
 
-                Console.WriteLine($"{result.Point1} {result.Point2}");
+            Console.WriteLine($"{fp1} {fp2}");
+        }
+    }
+
+    private class NodePoint
+    {
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+
+    }
+
+    private class Graph<T>
+    {
+        public Graph()
+        {
+        }
+
+        public Graph(IEnumerable<T> vertices, IEnumerable<Tuple<T, T>> edges)
+        {
+            foreach (var vertex in vertices)
+                AddVertex(vertex);
+
+            foreach (var edge in edges)
+                AddEdge(edge);
+        }
+
+        public Dictionary<T, HashSet<T>> AdjacencyList { get; } = new Dictionary<T, HashSet<T>>();
+
+        public void AddVertex(T vertex)
+        {
+            AdjacencyList[vertex] = new HashSet<T>();
+        }
+
+        public void AddEdge(Tuple<T, T> edge)
+        {
+            if (AdjacencyList.ContainsKey(edge.Item1) && AdjacencyList.ContainsKey(edge.Item2))
+            {
+                AdjacencyList[edge.Item1].Add(edge.Item2);
+                AdjacencyList[edge.Item2].Add(edge.Item1);
             }
         }
     }
-    private class NodePoint
+
+    private class Algorithms
     {
-        public int Point1 { get; set; }
-        public int Point2 { get; set; }
+        public List<T> BFS<T>(Graph<T> graph, T start, Action<T> preVisit = null)
+        {
+            var visited = new List<T>();
+
+            if (!graph.AdjacencyList.ContainsKey(start))
+                return visited;
+
+            var queue = new Queue<T>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+
+                if (visited.Contains(vertex))
+                    continue;
+
+                preVisit?.Invoke(vertex);
+
+                visited.Add(vertex);
+
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                    if (!visited.Contains(neighbor))
+                        queue.Enqueue(neighbor);
+            }
+
+            return visited;
+        }
+
+        public Func<T, IList<T>> ShortestPathFunction<T>(Graph<T> graph, T start)
+        {
+            var previous = new Dictionary<T, T>();
+
+            var queue = new Queue<T>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                {
+                    if (previous.ContainsKey(neighbor))
+                        continue;
+
+                    previous[neighbor] = vertex;
+                    queue.Enqueue(neighbor);
+                }
+            }
+
+            Func<T, IList<T>> shortestPath = v => {
+                var path = new List<T> { };
+
+                var current = v;
+                while (!current.Equals(start))
+                {
+                    path.Add(current);
+                    current = previous[current];
+                };
+
+                path.Add(start);
+                path.Reverse();
+
+                return path;
+            };
+
+            return shortestPath;
+        }
     }
 }
